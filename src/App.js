4808c90a205d84e592610c4de2438f2514a33c1b
lkrogers <lkrogers29@gmail.com>
2019-11-06 11:24:32 +0000

    Merge branch 'master' of C:\Users\lkrog\PhpstormProjects\CS353TeamProject with conflicts.

--- src/App.js
+++ src/App.js
@@ -2,190 +2,111 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class LoginPopUp extends React.Component {
-    render() {
-        return (
-        <div className="login">
-            <form>
-                <label>Username </label>
-                <input type="text" name="username" />
-                <br/>
-                <label>Password</label>
-                <input type="text" name="password" />
-            </form>
-        </div>
-        )
+class LoginForm extends React.Component{
+    constructor(props) {
+        super (props);
+        this.state = {
+            username: '',
+            password: ''
+        };
     }
-}
+    handleSubmit = (event) => {
+        event.preventDefault();
+        alert("You are submitting username: " + this.state.username + ", and password: " + this.state.password);
+    }
+    handleChange = (event) => {
+        this.setState({[event.target.name]: event.target.value});
+    }
 
-class GoalsView extends React.Component {
     render() {
         return (
-            <div id="results">
-                will show the results of the users nutrition.
+            <div id="logIn">
+                <form onSubmit={this.handleSubmit}>
+                    <label>Username </label>
+                    <input type="text" name="username" onChange={this.handleChange}/><br/>
+                    <label>Password </label>
+                    <input type="password" name="password" onChange={this.handleChange}/><br/>
+                    <input type="submit" value="Submit"/>
+                </form>
             </div>
         );
     }
 }
 
-class HomeView extends React.Component {
-    render() {
-        return (
-            <div className="home">
-                <div className="home-illustration">
-                    Cute big illustration
-                </div>
-                <div className="home-titles">
-                    <h2>Plan your meals easily<br/>
-                        Track your nutrition.<br/>
-                        Get your goals.
-                    </h2>
-                    <p>
-                        Create your flexible mealplan and input your actual meals.
-                        Track your nutrition with our simplified graphics and adjust your plan to get your goals.
-                    </p>
-                    <button className="btn-login">CREATE YOUR FREE ACCOUNT</button>
-                </div>
-            </div>
-        );
-    }
+function GoalsView() {
+  return (
+      <div id="results">
+        will show the results of the users nutrition.
+      </div>
+  );
 }
 
-class PlannerView extends React.Component {
-    render() {
-        return (
-            <div id="main">
-                <h1>Plan My Meal</h1>
-                View Profile
-                <SearchBar/>
-                <WeekPlan/>
-                <NutriScore/>
-            </div>
-        );
-    }
+function PlannerView() {
+  return (
+      <div id="main">
+        <h1>Plan My Meal</h1>
+        View Profile
+        <SearchBar/>
+        <WeekPlan/>
+        <Nutrition/>
+      </div>
+  );
 }
 
-class WeekPlan extends React.Component{
-    render() {
-        return (
-            <table id="plan">
-                <tr>
-                    <th>Meal</th>
-                    <th>Monday</th>
-                    <th>Tuesday</th>
-                    <th>Wednesday</th>
-                    <th>Thursday</th>
-                    <th>Friday</th>
-                    <th>Saturday</th>
-                    <th>Sunday</th>
 
-                </tr>
-                <tr>
-                    <th>Meal 1</th>
-                </tr>
-                <tr>
-                    <th>Meal 2</th>
-                </tr>
-                <tr>
-                    <th>Meal 3</th>
-                </tr>
-            </table>
-        );
-    }
+function WeekPlan() {
+  return (
+      <table id="plan">
+        <tr>
+          <th>Meal</th>
+          <th>Monday</th>
+          <th>Tuesday</th>
+          <th>Wednesday</th>
+          <th>Thursday</th>
+          <th>Friday</th>
+          <th>Saturday</th>
+          <th>Sunday</th>
+
+        </tr>
+        <tr>
+          <th>Meal 1</th>
+        </tr>
+        <tr>
+          <th>Meal 2</th>
+        </tr>
+        <tr>
+          <th>Meal 3</th>
+        </tr>
+      </table>
+  );
 }
 
-class SearchBar extends React.Component {
-    render() {
-        return (
-            <div className="search">
-                Search Meal:
-            </div>
-        );
-    }
+function SearchBar() {
+  return (
+      <div className="search">
+        Search Meal:
+      </div>
+  );
 }
 
-class NutriScore extends React.Component {
-    render() {
-        return (
-            <div id="results">
-                will show the results of the users nutrition.
-            </div>
-        );
-    }
+function Nutrition() {
+  return (
+      <div id="results">
+        will show the results of the users nutrition.
+      </div>
+  );
 }
 
-class Header extends React.Component {
-    render() {
-        return (
-            <header className="header">
-                <span className="header-brand">
-                    planmy.ml
-                </span>
-                <span className="header-link">
-                    Mealplans
-                </span>
-                <span className="header-link">
-                    Recipes
-                </span>
-                <span className="header-link">
-                    Goals
-                </span>
-                <span className="header-link">
-                    About Us
-                </span>
-                <span className="header-login">
-                    LOGIN
-                </span>
-            </header>
-        );
-    }
-}
 
-class Footer extends React.Component {
-    render() {
-        return (
-            <footer className="footer">
-                <div className="footer-item">
-                    <p>planmy.ml</p>
-                    <p>Mealplans</p>
-                    <p>Recipes</p>
-                    <p>Goals</p>
-                    <p>About Us</p>
-                </div>
-
-                <div className="footer-item">
-                    cute ilustration
-                </div>
-
-                <div className="footer-item">
-                    <p>Contact & Suport</p>
-                    <p>FAQ</p>
-                    <p>Email</p>
-                    <p>Instagram</p>
-                </div>
-
-            </footer>
-        );
-    }
-}
-
-function View() {
-    return (
-        <div className="view">
-            <LoginPopUp/>
-            <HomeView/>
-        </div>
-    );
-}
 
 function App() {
-    return (
-        <div className="App">
-            <Header/>
-            <View/>
-            <Footer/>
-        </div>
-    );
+  return (
+      <div className="App">
+        <LoginForm/>
+        <PlannerView/>
+        <GoalsView/>
+      </div>
+  );
 }
 
 export default App;
