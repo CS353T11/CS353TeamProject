67856d3f71d5bcf5d90da7b7028bfca2ad76c3e2
Enming <821116016@qq.com>
2019-11-08 13:36:59 +0000

    'Create new account' button only display when no user loggedin

--- src/components/home/homeview.js
+++ src/components/home/homeview.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import LoginPopUp from '../authentication/loginPopUp';
+import firebase from 'firebase';
 
 export default class HomeView extends React.Component {
+    state = {
+        user: null,
+        error: null,
+    }
+
+    componentDidMount() {
+        firebase.auth().onAuthStateChanged(user => {
+            if (user) {
+                this.setState({ user })
+            }
+        })
+    }
+
     render() {
         return (
             <div className="view">
+                {/* loginPopUp can only show in home page, because of the structure */}
                 <Switch><Route path='/home/login' component={LoginPopUp} /></Switch>
                 <div className="home">
                     <div className="home-illustration">
@@ -20,7 +35,7 @@ export default class HomeView extends React.Component {
                             Create your flexible mealplan and input your actual meals.
                             Track your nutrition with our simplified graphics and adjust your plan to get your goals.
                     </p>
-                        <button className="btn-login">CREATE YOUR FREE ACCOUNT</button>
+                        {this.state.user ? null : <button className="btn-login">CREATE YOUR FREE ACCOUNT</button>}
                     </div>
                 </div>
             </div>
