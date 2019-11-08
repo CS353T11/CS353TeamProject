7ee27a213af0eea021d9c87f98534ca3cf1be615
Enming <821116016@qq.com>
2019-11-08 17:35:12 +0000

    'Create new account' button only display when no user loggedin

--- src/components/home/homeview.js
+++ src/components/home/homeview.js
@@ -1,11 +1,26 @@
 import React from 'react';
-import {Switch, Route, NavLink} from 'react-router-dom';
+import { Switch, Route, NavLink } from 'react-router-dom';
 import LoginPopUp from '../authentication/loginPopUp';
+import firebase from 'firebase';
 
 export default class HomeView extends React.Component {
+    state = {
+        user: null,
+        error: null,
+    }
+
+    componentDidMount() {
+        firebase.auth().onAuthStateChanged(user => {
+            if (user) {
+                this.setState({ user })
+            }
+        })
+    }
+
     render() {
         return (
             <div className="view">
+                {/* loginPopUp can only show in home page, because of the structure */}
                 <Switch><Route path='/home/login' component={LoginPopUp} /></Switch>
                 <div className="home">
                     <div className="home-illustration">
@@ -15,12 +30,12 @@ export default class HomeView extends React.Component {
                         <h2 className="title">Plan your meals easily<br />
                             Track your nutrition.<br />
                             Get your <b className="bold">goals.</b>
-                    </h2>
+                        </h2>
                         <p className="subtitle">
                             Create your flexible mealplan and input your actual meals.<br />
                             Track your nutrition with our simplified graphics and adjust <br /> your plan to get your goals.
                     </p>
-                        <NavLink className="btn-login" to='/home/register'>CREATE YOUR ACCOUNT</NavLink>
+                        {this.state.user ? null : <NavLink className="btn-login" to='/home/register'>CREATE YOUR ACCOUNT</NavLink>}
                     </div>
                 </div>
             </div>
