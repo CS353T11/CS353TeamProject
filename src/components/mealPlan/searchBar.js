69e91ec364c80e4711ab4dd5b28e888244a84a79
Aaron Scully <aaronscully2@gmail.com>
2019-11-13 11:56:38 +0000

    small searchbar changes

--- src/components/mealPlan/searchBar.js
+++ src/components/mealPlan/searchBar.js
@@ -1,12 +1,41 @@
 import React from 'react';
 import {TextInput} from 'react-materialize';
+import axios from 'axios'
 export default class SearchBar extends React.Component {
+
     constructor(props){
         super(props);
     }
     keyPressed(event) {
         if (event.key === "Enter") {
-            alert("Yooooo");
+            alert("Lets a GOOoooo");
+            //making a hard coded post request for an apple
+            axios.get("https://api.edamam.com/api/food-database/parser?ingr=apple&app_id=9ccfd3ea&app_key=422e0ba66ae6c563f47a9fe391a437f0")
+                .then(function(response){
+                    console.log("----------Response JSON----------");
+                    console.table(response);
+                    console.log("---------------------------------");
+                    var obj=response;
+                    //var objs;
+                    //console.log(data.hints[0].measures[1].label);
+                    var foodId;
+                    {Object.keys().map()}
+
+                    /*$.each(obj, function (key, value) {
+                        if(key=="hints"){
+                            //objs=value[0];
+                            console.log("----------Food JSON----------");
+                            console.log(value[0]);
+                            console.log("-----------------------------");
+                            foodId=value[0].food.foodId;
+                            console.log("Food Id: "+foodId);
+                        }
+                    });*/
+                })
+                .catch(function(error){
+                    console.log(error);
+            })
+
         }
     }
 
--- test Api/testingJs.js
+++ test Api/testingJs.js
@@ -8,7 +8,7 @@ function testy(food){
     $('#foodName').html(food);
 
     var urlFood=encodeURI(food);
-
+/*
     //Using Jquery's Get request
     $.get( "https://api.edamam.com/api/food-database/parser?ingr="+urlFood+"&app_id=9ccfd3ea&app_key=422e0ba66ae6c563f47a9fe391a437f0", function( data, status) {
         alert(status);
@@ -73,7 +73,67 @@ function testy(food){
         //var testyJSON=JSON.parse(data);
         //console.log(testyJSON);
         alert( "Load was performed.");
-    });
+    }); */
 
+    $.get( "https://api.edamam.com/api/food-database/parser?ingr="+urlFood+"&app_id=9ccfd3ea&app_key=422e0ba66ae6c563f47a9fe391a437f0", function( data, status) {
+        alert(status);
+        console.log("----------Response JSON----------");
+        console.log(data);
+        console.log("---------------------------------");
+        var obj=data;
+        //var objs;
+        //console.log(data.hints[0].measures[1].label);
+        var foodId;
+
+        $.each(obj, function (key, value) {
+            if(key=="hints"){
+                //objs=value[0];
+                console.log("----------Food JSON----------");
+                console.log(value[0]);
+                console.log("-----------------------------");
+                foodId=value[0].food.foodId;
+                console.log("Food Id: "+foodId);
+            }
+        });
+
+        var nutrientReq={
+            "ingredients": [
+                {
+                    "quantity": 100,
+                    "measureURI": "http://www.edamam.com/ontologies/edamam.owl#Measure_gram",
+                    "foodId": foodId,
+                    "qualifiers": [ "http://www.edamam.com/ontologies/edamam.owl#Qualifier_large" ]
+                }
+            ]
+        };
+
+        $.ajax({
+            type: "POST",
+            data: JSON.stringify(nutrientReq),
+            url: "https://api.edamam.com/api/food-database/nutrients?app_id=9ccfd3ea&app_key=422e0ba66ae6c563f47a9fe391a437f0",
+            contentType: "application/json",
+            success: function(nutrData){
+                console.log("----------Nutrient JSON----------");
+                console.log(nutrData);
+                console.log("---------------------------------");
+
+                var fullNutrData=nutrData.totalNutrients;
+                $.each(fullNutrData,function (key,value) {
+
+                    if(key=="ENERC_KCAL"){
+                        $('#cal').html(""+value.quantity+value.unit);
+                    }else if(key=="FAT"){
+                        $('#fat').html(""+value.quantity+value.unit);
+                    }else if(key=="PROCNT"){
+                        $('#protein').html(""+value.quantity+value.unit);
+                    }else if(key=="CHOCDF"){
+                        $('#carbs').html(""+value.quantity+value.unit);
+                    }
+                });
+            }
+        });
+
+        alert( "Load was performed.");
+    });
 }
 
