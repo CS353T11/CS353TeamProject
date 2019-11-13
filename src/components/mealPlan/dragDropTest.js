a71534515bbd52d7f57051ca885835ba09dc9350
AJ-Scully <aaronscully2@gmail.com>
2019-11-13 21:24:47 +0000

    Drag drop test component

---
+++ src/components/mealPlan/dragDropTest.js
@@ -0,0 +1,10 @@
+import React from 'react';
+export default class DragDrop extends React.Component {
+    render() {
+        return (
+            <div className="dragDropTest">
+                Wowee
+            </div>
+        );
+    }
+}
