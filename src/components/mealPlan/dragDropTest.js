e6903a87c309560d5e52f7dfdcbad13f8d3684fa
Aaron Scully <aaronscully2@gmail.com>
2019-11-27 15:34:31 +0000

    List functionality for multiple dropped objects

--- src/components/mealPlan/dragDropTest.js
+++ src/components/mealPlan/dragDropTest.js
@@ -1,58 +1,57 @@
 import React from 'react';
+
 export default class DragDrop extends React.Component {
-    constructor(props){
-        super(props);
-        this.state={
-            foodList:[{label: '', cal:'', fat:'', pro:'', carbs:''},]
-        }
-    }
+	constructor(props) {
+		super(props);
+		this.state = {
+			foodList: []
+		}
+	}
 
-    drop = e =>{
-        e.preventDefault();
-        const foodJSON=e.dataTransfer.getData('foodJSON');
-        const foodObj=JSON.parse(foodJSON);
+	drop = e => {
+		e.preventDefault();
+		const foodJSON = e.dataTransfer.getData('foodJSON');
+		const foodObj = JSON.parse(foodJSON);
 
-        this.setState(prevState =>{
-            let foodList=Object.assign({}, prevState.foodList);
-            let temp={
-            	label:foodObj.label,
-				cal:foodObj.cal,
-				fat:foodObj.fat,
-				pro:foodObj.pro,
-				carbs:foodObj.carbs
+		this.setState(prevState => {
+			let foodList = prevState.foodList;
+			let temp = {
+				label: foodObj.label,
+				cal: foodObj.cal,
+				fat: foodObj.fat,
+				pro: foodObj.pro,
+				carbs: foodObj.carbs
 			};
-            // var ar=[{wow:''}];
-			// ar=ar.concat(temp);
-			//foodList.concat(temp);
 			console.log(temp);
+			foodList.push(temp);
 			console.log(foodList);
 			return {foodList};
-        })
-        //e.target.appendChild(card);
-        //this.setState({wow[0]=car});
-        //this.state.wow[0]=car;
-    }
+		})
+		//e.target.appendChild(card);
+		//this.setState({wow[0]=car});
+		//this.state.wow[0]=car;
+	};
 
-    dragOver = e =>{
-        e.preventDefault();
-    }
+	dragOver = e => {
+		e.preventDefault();
+	};
 
-    render(){
-        return (
-            <div className={this.props.className}
-                 id={this.props.id}
-                 onDrop={this.drop}
-                 onDragOver={this.dragOver}
-            >
-                <ul>
-                    <li>{this.state.foodList[0].label}</li>
-                    <li>{this.state.foodList[0].cal}</li>
-                    <li>{this.state.foodList[0].fat}</li>
-                    <li>{this.state.foodList[0].carbs}</li>
-                    <li>{this.state.foodList[0].pro}</li>
-                </ul>
-            </div>
-        );
-    }
+	render() {
+		return (
+			<div className={this.props.className}
+				 id={this.props.id}
+				 onDrop={this.drop}
+				 onDragOver={this.dragOver}
+			>
+				<ul>
+					<li>{this.state.foodList[0].label}</li>
+					<li>{this.state.foodList[0].cal}</li>
+					<li>{this.state.foodList[0].fat}</li>
+					<li>{this.state.foodList[0].carbs}</li>
+					<li>{this.state.foodList[0].pro}</li>
+				</ul>
+			</div>
+		);
+	}
 
 }
