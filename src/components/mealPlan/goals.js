f347a25b9112c3f3466e99277cbd0e27642c56f9
Jake <ekajb97@gmail.com>
2019-11-28 17:40:15 +0000

    changed radio buttons to drop box, working on progress page

--- src/components/mealPlan/goals.js
+++ src/components/mealPlan/goals.js
@@ -1,48 +1,44 @@
 import React from 'react'
 import Avatar from "../../images/avatar.svg";
-import {RadioGroup} from 'react-materialize';
+import {RadioGroup, Select} from 'react-materialize';
 export default class Goals extends React.Component {
     render() {
         return (
             <div id="results">
                 <div className="user-details">
                     <div className="diet">
-                        <RadioGroup
-                            name="diet" withGap label="Diets" options={[
-                            {label: 'Maintain Weight',value: 'op1'},
-                            {label: 'Loose Weight',value: 'op2'},
-                            {label: 'Gain Weight',value: 'op3'},
-                            {label: 'High Protein',value: 'op4'},
-                            {label: 'Low Carbs',value: 'op5'}]}>
-                        </RadioGroup>
-                        {/*<form action="#">
-                            <p>
-                                <label>
-                                    <input name="group1" type="radio" checked/>
-                                    <span>Red</span>
-                                </label>
-                            </p>
-                            <p>
-                                <label>
-                                    <input name="group1" type="radio"/>
-                                    <span>Yellow</span>
-                                </label>
-                            </p>
-                            <p>
-                                <label>
-                                    <input className="with-gap" name="group1" type="radio"/>
-                                    <span>Green</span>
-                                </label>
-                            </p>
-                            <p>
-                                <label>
-                                    <input name="group1" type="radio" disabled="disabled"/>
-                                    <span>Brown</span>
-                                </label>
-                            </p>
-                        </form>*/}
-
+                    <br/>
+                        <Select
+                            onChange={function noRefCheck(){}}
+                            options={{
+                                classes: '',
+                                dropdownOptions: {alignment: 'left', autoTrigger: true, closeOnClick: true, constrainWidth: true, container: null, coverTrigger: true, hover: false, inDuration: 150, onCloseEnd: null, onCloseStart: null, onOpenEnd: null, onOpenStart: null, outDuration: 250}
+                            }} value="">
+                            <option
+                                disabled
+                                value=""
+                            >
+                                Diet Plan
+                            </option>
+                            <option value="maintain">
+                                Maintain Weight
+                            </option>
+                            <option value="loose">
+                                Loose Weight
+                            </option>
+                            <option value="gain">
+                                Gain Weight
+                            </option>
+                            <option value="protein">
+                                High Protein
+                            </option>
+                        </Select>
                     </div>
+                </div>
+                <div className="maintain">
+                    if male: (66 + (6.3x( weightkg x 2.205)) + (12.9 x (heightcm / 2.54)) - (6.8 x age))
+                    if female (665 +(4.3 x ( weightkg x 2.205)) + (4.7 X (heightcm / 2.54)) - (4.7 x age))
+
                 </div>
             </div>
         );
--- src/components/userInfo/profile.js
+++ src/components/userInfo/profile.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import firebase from '../firebase/firebase';
 import Avatar from '../../images/avatar.svg';
-import {RadioGroup} from 'react-materialize';
+import {RadioGroup, Select} from 'react-materialize';
 
 
 export default class Profile extends React.Component {
@@ -64,7 +64,7 @@ export default class Profile extends React.Component {
                     <div className="user-details">
                         <div className="profile-id">
                             <img src={Avatar} alt="avatar"></img><br></br>
-                            <h2 className="title"><p className="bold">{name}</p></h2>
+                            <h1 className="title"><h1 className="bold">{name}</h1></h1>
                             <div><b className="bold">email</b></div>
                             <div><b className="bold">password</b></div>
                             <button className="submit">edit details</button>
@@ -72,35 +72,70 @@ export default class Profile extends React.Component {
                     </div>
                     <div className="extra-details">
                         <form onSubmit={this.handleSubmit} className="form">
-                            <h1>User Details</h1>
+                            <div className="hdr">User Details</div>
                             <div className="table">
                                 <div className="row">
-                                    <div className="cell">Name</div>
+                                    <div className="cell">Name:</div>
                                     <div className="cell"><input id="name" required className="input" type="text" placeholder="Name" onChange={this.handleChange} value={name}></input></div>
                                 </div>
                                 <div className="row">
-                                    <div className="cell">Age</div>
+                                    <div className="cell">Age:</div>
                                     <div className="cell"><input id="age" required className="input" type="text" placeholder="Age" onChange={this.handleChange} value={age}></input></div>
                                 </div>
                                 <div className="row">
-                                    <div className="cell">Gender</div>
-                                    <RadioGroup
-                                        name="gender" withGap label="Gender" options={[
-                                        {label: 'Male',value: 'op1'},
-                                        {label: 'Female',value: 'op2'}]}>
-                                    </RadioGroup>
+                                    <div className="cell">Gender:</div>
+                                    <Select
+                                        onChange={function noRefCheck(){}}
+                                        options={{
+                                            classes: '',
+                                            dropdownOptions: {alignment: 'left', autoTrigger: true, closeOnClick: true, constrainWidth: true, container: null, coverTrigger: true, hover: false, inDuration: 150, onCloseEnd: null, onCloseStart: null, onOpenEnd: null, onOpenStart: null, outDuration: 250}
+                                        }} value="">
+                                        <option
+                                            disabled
+                                            value=""
+                                        >
+                                            Gender
+                                        </option>
+                                        <option value="Male">
+                                            Male
+                                        </option>
+                                        <option value="Female">
+                                            Female
+                                        </option>
+                                    </Select>
                                 </div>
                                 <div className="row">
-                                    <div className="cell">Height</div>
+                                    <div className="cell">Height (cm):</div>
                                     <div className="cell"><input id="height" required className="input" type="text" placeholder="Height" onChange={this.handleChange} value={height}></input></div>
                                 </div>
                                 <div className="row">
-                                    <div className="cell">Weight</div>
+                                    <div className="cell">Weight (kg):</div>
                                     <div className="cell"><input id="weight" required className="input" type="text" placeholder="Weight" onChange={this.handleChange} value={weight}></input></div>
                                 </div>
                                 <div className="row">
-                                    <div className="cell">Activity level</div>
-                                    <div className="cell"><input id="activityLevel" required className="input" type="text" placeholder="Activity Level" onChange={this.handleChange} value={activityLevel}></input></div>
+                                    <div className="cell">Activity level:</div>
+                                    <Select
+                                        onChange={function noRefCheck(){}}
+                                        options={{
+                                            classes: '',
+                                            dropdownOptions: {alignment: 'left', autoTrigger: true, closeOnClick: true, constrainWidth: true, container: null, coverTrigger: true, hover: false, inDuration: 150, onCloseEnd: null, onCloseStart: null, onOpenEnd: null, onOpenStart: null, outDuration: 250}
+                                        }} value="">
+                                        <option
+                                            disabled
+                                            value=""
+                                        >
+                                            Activity Level
+                                        </option>
+                                        <option value="Low">
+                                            Low
+                                        </option>
+                                        <option value="Moderate">
+                                            Moderate
+                                        </option>
+                                        <option value="High">
+                                            High
+                                        </option>
+                                    </Select>
                                 </div>
                             </div>
                         <div>
