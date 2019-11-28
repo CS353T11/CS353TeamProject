89453a8ab4fa14372bab5ae0ce0ee9d60d65d035
Jake <ekajb97@gmail.com>
2019-11-28 12:47:48 +0000

    Added radio button to progress page

--- src/components/mealPlan/goals.js
+++ src/components/mealPlan/goals.js
@@ -1,39 +1,47 @@
 import React from 'react'
 import Avatar from "../../images/avatar.svg";
+import {RadioGroup} from 'react-materialize';
 export default class Goals extends React.Component {
     render() {
         return (
             <div id="results">
                 <div className="user-details">
                     <div className="diet">
-                        {/* <RadioGroup>
-                            name="diet" withGap label="Diets" value="op1"
-                            options={[{label: 'Maintain Weight',value: 'op1'},{label: 'Loose Weight',value: 'op2'},{label: 'Gain Weight',value: 'op3'},
-                                {label: 'High Protein',value: 'op4'},{label: 'Low Carbs',value: 'op5'}]}
-                        </RadioGroup>*/}
-                        <form>
-
-                            <div className="form-check">
+                        <RadioGroup
+                            name="diet" withGap label="Diets" options={[
+                            {label: 'Maintain Weight',value: 'op1'},
+                            {label: 'Loose Weight',value: 'op2'},
+                            {label: 'Gain Weight',value: 'op3'},
+                            {label: 'High Protein',value: 'op4'},
+                            {label: 'Low Carbs',value: 'op5'}]}>
+                        </RadioGroup>
+                        {/*<form action="#">
+                            <p>
                                 <label>
-                                    <input type="radio" name="dietChoice" value="option4" checked={true} className="form-check-input"/>
-                                    High Protein
+                                    <input name="group1" type="radio" checked/>
+                                    <span>Red</span>
                                 </label>
-                            </div>
-
-                            <div className="form-check">
+                            </p>
+                            <p>
                                 <label>
-                                    <input type="radio" name="dietChoice" value="option5" checked={true} className="form-check-input"/>
-                                    Low Carbs
+                                    <input name="group1" type="radio"/>
+                                    <span>Yellow</span>
                                 </label>
-                            </div>
+                            </p>
+                            <p>
+                                <label>
+                                    <input className="with-gap" name="group1" type="radio"/>
+                                    <span>Green</span>
+                                </label>
+                            </p>
+                            <p>
+                                <label>
+                                    <input name="group1" type="radio" disabled="disabled"/>
+                                    <span>Brown</span>
+                                </label>
+                            </p>
+                        </form>*/}
 
-                            <div className="form-group">
-                                <button className="btn btn-primary mt-2" type="submit">
-                                    Save
-                                </button>
-                            </div>
-
-                        </form>
                     </div>
                 </div>
             </div>
