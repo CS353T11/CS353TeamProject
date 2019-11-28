ce4afbd0239dacb87e1d164df2d68644484011b7
Jake <ekajb97@gmail.com>
2019-11-28 16:04:19 +0000

    Fixed the orientation of the profile page, radio buttons still not functional atm.

--- src/components/userInfo/profile.js
+++ src/components/userInfo/profile.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import firebase from '../firebase/firebase';
 import Avatar from '../../images/avatar.svg';
+import {RadioGroup} from 'react-materialize';
+
 
 export default class Profile extends React.Component {
     state = {
@@ -59,56 +61,56 @@ export default class Profile extends React.Component {
         const { name, age, height, weight, activityLevel, } = this.state;
         return (
             <div className="profile">
-                <form onSubmit={this.handleSubmit} className="form">
                     <div className="user-details">
                         <div className="profile-id">
                             <img src={Avatar} alt="avatar"></img><br></br>
-        <h2 className="title"><p className="bold">{name}</p></h2>
+                            <h2 className="title"><p className="bold">{name}</p></h2>
                             <div><b className="bold">email</b></div>
                             <div><b className="bold">password</b></div>
-                            <button>edit details</button>
+                            <button className="submit">edit details</button>
                         </div>
                     </div>
                     <div className="extra-details">
-                        <h1>User Details</h1>
-                        <div className="table">
-                            <div className="row">
-                                <div className="cell">Name</div>
-                                <div className="cell"><input id="name" required className="input" type="text" placeholder="Name" onChange={this.handleChange} value={name}></input></div>
+                        <form onSubmit={this.handleSubmit} className="form">
+                            <h1>User Details</h1>
+                            <div className="table">
+                                <div className="row">
+                                    <div className="cell">Name</div>
+                                    <div className="cell"><input id="name" required className="input" type="text" placeholder="Name" onChange={this.handleChange} value={name}></input></div>
+                                </div>
+                                <div className="row">
+                                    <div className="cell">Age</div>
+                                    <div className="cell"><input id="age" required className="input" type="text" placeholder="Age" onChange={this.handleChange} value={age}></input></div>
+                                </div>
+                                <div className="row">
+                                    <div className="cell">Gender</div>
+                                    <RadioGroup
+                                        name="gender" withGap label="Gender" options={[
+                                        {label: 'Male',value: 'op1'},
+                                        {label: 'Female',value: 'op2'}]}>
+                                    </RadioGroup>
+                                </div>
+                                <div className="row">
+                                    <div className="cell">Height</div>
+                                    <div className="cell"><input id="height" required className="input" type="text" placeholder="Height" onChange={this.handleChange} value={height}></input></div>
+                                </div>
+                                <div className="row">
+                                    <div className="cell">Weight</div>
+                                    <div className="cell"><input id="weight" required className="input" type="text" placeholder="Weight" onChange={this.handleChange} value={weight}></input></div>
+                                </div>
+                                <div className="row">
+                                    <div className="cell">Activity level</div>
+                                    <div className="cell"><input id="activityLevel" required className="input" type="text" placeholder="Activity Level" onChange={this.handleChange} value={activityLevel}></input></div>
+                                </div>
                             </div>
-                            <div className="row">
-                                <div className="cell">Age</div>
-                                <div className="cell"><input id="age" required className="input" type="text" placeholder="Age" onChange={this.handleChange} value={age}></input></div>
-                            </div>
-                            <div className="row">
-                                <div className="cell">Gender</div>
-                                {/* <div className="cell">
-                                <select id="gender" onSelect={this.handleChange}>
-                                    <option value='1'>Male</option>
-                                    <option value='0'>Female</option>
-                                </select> </div> */}
-                            </div>
-                            <div className="row">
-                                <div className="cell">Height</div>
-                                <div className="cell"><input id="height" required className="input" type="text" placeholder="Height" onChange={this.handleChange} value={height}></input></div>
-                            </div>
-                            <div className="row">
-                                <div className="cell">Weight</div>
-                                <div className="cell"><input id="weight" required className="input" type="text" placeholder="Weight" onChange={this.handleChange} value={weight}></input></div>
-                            </div>
-                            <div className="row">
-                                <div className="cell">Activity level</div>
-                                <div className="cell"><input id="activityLevel" required className="input" type="text" placeholder="Activity Level" onChange={this.handleChange} value={activityLevel}></input></div>
-                            </div>
-                        </div>
                         <div>
-                            <button type="submit">Submit</button>
+                            <button type="submit" className="submit">Submit</button>
                             <div>
                                 {this.state.error ? <p>{this.state.error.message}</p> : null}
                             </div>
                         </div>
-                    </div>
-                </form>
+                    </form>
+                </div>
                 <div className="achievements">
                     Achievements
                     </div>
