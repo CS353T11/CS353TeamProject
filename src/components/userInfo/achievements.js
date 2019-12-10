0e4a0763b103948c895472543febf76c3c403c2a
Jake <ekajb97@gmail.com>
2019-12-10 19:50:00 +0000

    Reverted back to functional profile editing for Ayman to remerge code.

---
+++ src/components/userInfo/achievements.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default class Achievements extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        };
+    }
+
+    render() {
+        return (
+            <div className="profile-achievements">
+                <h3 className="title">Achievements</h3>
+            </div>
+        );
+    };
+
+}
