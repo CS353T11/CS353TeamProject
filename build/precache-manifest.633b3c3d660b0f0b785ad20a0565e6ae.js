4ac5617cea5f4e1219199a7ca84233b591d1255c
AJ-Scully <aaronscully2@gmail.com>
2019-12-14 22:47:22 +0000

    Created new build and file exceptions for .gitignore

---
+++ build/precache-manifest.633b3c3d660b0f0b785ad20a0565e6ae.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95c5a30de876c49bdef347894b9ccb8a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a77146babf22f80954b",
+    "url": "/static/css/main.93775f15.chunk.css"
+  },
+  {
+    "revision": "1eda5b2253cbe2ab6209",
+    "url": "/static/js/2.8f36eaf7.chunk.js"
+  },
+  {
+    "revision": "0a77146babf22f80954b",
+    "url": "/static/js/main.86ea15cd.chunk.js"
+  },
+  {
+    "revision": "2fa82451a36aee16d79b",
+    "url": "/static/js/runtime-main.e2b9cd8b.js"
+  },
+  {
+    "revision": "97b8107d2da499d423b23383dd2b37ea",
+    "url": "/static/media/avatar.97b8107d.svg"
+  },
+  {
+    "revision": "d7f52adc48801437a79a33ec24809a2f",
+    "url": "/static/media/homepage-gif.d7f52adc.gif"
+  },
+  {
+    "revision": "113e4202b0f1be7f9875710583a2d85f",
+    "url": "/static/media/loading.113e4202.svg"
+  },
+  {
+    "revision": "1550fc64211be024a5a47bb272db8075",
+    "url": "/static/media/profile.1550fc64.svg"
+  }
+]);
