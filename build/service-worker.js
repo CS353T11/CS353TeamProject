3ac2ed8dc8002bff38f27c73183b4f5e8a0d2ad2
Aaron Scully <aaronscully2@gmail.com>
2020-09-04 11:08:38 +0100

    -fix self promotion bug ;)
    Signed-off-by: aaronscully2@gmail.com <CatherineHalsey2492QWASZX>

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.aae0bb641a190e2b26df3253197404d5.js"
+  "/precache-manifest.7e6fb9f19443b053e24edaccc70f3744.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
